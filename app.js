de65dab575f0180824fdef13ba3aad80dc0ab0f0
Paulina Aranda <paulinaarandawastavino@gmail.com>
2025-02-02 14:37:54 -0300

    Creacion lista amigos

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigo = 0
+
+function agregarAmigo(){
+    let ingresarNombre = document.getElementById ('amigo').value;
+
+    if (ingresarNombre == ""){
+        alert ("Por favor, ingrese un nombre")
+    }else{
+        amigos.push(ingresarNombre)
+    }
+    limpiarCaja()
+    recorrerListaAmigos ()
+    
+    console.log(amigos)
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+   
+
+function recorrerListaAmigos(){
+    let lista = document.getElementById ("listaAmigos")
+    lista.innerHTML = ""
+    
+    for (indice = 0; indice < amigos.length; indice = indice + 1) {
+        let elementoLi = document.createElement( 'li' );
+        elementoLi.innerHTML = amigos [indice]
+        lista.appendChild( elementoLi );
+    } 
+}
